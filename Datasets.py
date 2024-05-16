d643132e82ca331504ab351fe770750a8588555a
Xiaochen Yu <104009397+xiaocheny1209@users.noreply.github.com>
2024-05-16 19:44:29 +0800

    finalize

--- Datasets.py
+++ Datasets.py
@@ -3,6 +3,8 @@ import torch
 from torch.utils.data import Dataset
 from tqdm import tqdm
 from collections import defaultdict
+import matplotlib.pyplot as plt
+import os
 
 class InitDataset():
     def __init__(self, rating_file='Epinion/processed_data/full_ratings2.csv', profile_file='Epinion/processed_data/profileLoc.csv', user_dict_file = 'Epinion/processed_data/user_dict.txt'):
@@ -10,7 +12,21 @@ class InitDataset():
         self.user_df, self.profile_user_df = self._get_user(profile_file)
         self.user_dict = self._get_user_dict(user_dict_file)
         self.filtered_rating_df = self._get_filtered_rating_df()
+        # _,self.location_user_df = self.get_location_group()
         print(int(self.filtered_rating_df['rating'].max()))
+
+    def describe_location(self):
+        x = self.profile_user_df.groupby("location")["username"].count().index.tolist()
+        print("x",x)
+        y_rating_cnts = self.profile_user_df.groupby("location")["username"].count().tolist()
+        print("y",y_rating_cnts)
+        location_dic = {}
+        for i in range (len(x)):
+            location_dic[x[i]] = y_rating_cnts[i]
+        location_dic = {k: v for k, v in sorted(location_dic.items(), key=lambda item: item[1])}
+        plt.pie(location_dic.values(), labels = location_dic.keys(), autopct='%1.1f%%',textprops={'size': 'smaller'})
+        # plt.legend(title = "Locations:")
+        plt.show()
 
     def describe(self):
         return int(self.filtered_rating_df['user'].max())+1, int(self.filtered_rating_df['item'].max())+1, int(self.filtered_rating_df['rating'].max())+1
@@ -33,7 +49,7 @@ class InitDataset():
             user_dict = eval(file_contents) # self.user_dict: key is userID and value is username
         return user_dict
     
-    def _get_filtered_rating_df(self, sample=15000): 
+    def _get_filtered_rating_df(self): 
         print('Filtering rating df ...')
         user_ratings_count = self.rating_df.groupby('user')['rating'].count()
         users_with_valid_rating = set(user_ratings_count[user_ratings_count >= 5].index) # users with at least 5 ratings
@@ -44,8 +60,90 @@ class InitDataset():
         
         filtered_rating_df = self.rating_df[self.rating_df['user'].isin(users_with_valid_rating & users_with_valid_profile)]
         # print('filtered rating df len =',filtered_rating_df['user'].nunique()) # 22732 
-        filtered_rating_df = filtered_rating_df.head(sample) # sample the first 15000 rows
-        return filtered_rating_df 
+        # filtered_rating_df = filtered_rating_df.head(15000) 
+        return filtered_rating_df
+
+        ##filter group evaluation user with >=5 ratings
+    def get_filtered_user_df(self, any_user_df):
+        any_user_df['username'] = any_user_df['username'].replace(self.user_dict)
+        valid_user = self.filtered_rating_df['user'].unique()
+        filtered_user_df = any_user_df[any_user_df['username'].isin((valid_user))]
+        filtered_user_df = filtered_user_df.rename(columns = {'username':'user'})
+        return filtered_user_df 
+    
+    ## user location matching, only>5 & notna
+    def get_location_group(self): 
+        if os.path.exists('filtered_u_df.csv'):
+            filtered_u_df = pd.read_csv('filtered_u_df.csv')
+        else:
+            profile_user_df_copy = self.profile_user_df.copy()     
+            profile_user_df_copy = profile_user_df_copy[['username','location']] 
+            profile_user_df_copy = profile_user_df_copy.rename({'username':'user'})
+            filtered_u_df = self.get_filtered_user_df(profile_user_df_copy)
+            filtered_u_df.to_csv('filtered_u_df.csv', index=False)  # Set index=False to avoid saving row indices
+        return filtered_u_df
+    
+    ## user title matching, only>5 & notna
+    def get_title_group(self): 
+        if os.path.exists('filtered_u_df2.csv'):
+            filtered_u_df = pd.read_csv('filtered_u_df2.csv')
+        else:
+            profile_user_df_copy = self.profile_user_df.copy()     
+            profile_user_df_copy = profile_user_df_copy[['username','title']] 
+            profile_user_df_copy = profile_user_df_copy.rename({'username':'user'})
+            filtered_u_df = self.get_filtered_user_df(profile_user_df_copy)
+            filtered_u_df.to_csv('filtered_u_df2.csv', index=False)  # Set index=False to avoid saving row indices
+        return filtered_u_df
+            
+    ## new test set with different user, LOO
+    def create_test_location(self):
+        print('Getting test data with location ...')
+        train_set, validation_set, test_set = [], [], []
+        location_df = self.get_title_group()
+        print('location', location_df)
+        rating_df = self.filtered_rating_df
+        print('rating', rating_df)
+        merged_df = pd.merge(location_df, rating_df, on='user', how='inner')
+        
+        print("location group prev",merged_df)
+        grouped = merged_df.groupby('user')
+        # print("location group",g)
+
+        # Function to split data for each user
+        def split_user_data(user_df):
+            test_data = user_df.iloc[-1:]
+            return test_data
+
+        # Iterate over groups
+        print("grouped", len(grouped))
+        for _, group_df in tqdm(grouped):
+            test_data = split_user_data(group_df)
+            # train_set.append(train_data)
+            # validation_set.append(validation_data)
+            test_set.append(test_data)
+        # train_df = pd.concat(train_set, ignore_index=True)
+        # validation_df = pd.concat(validation_set, ignore_index=True)
+        test_df = pd.concat(test_set, ignore_index=True)
+        
+        return test_df
+    
+
+    def create_test_title(self):
+        print('Getting test data with title ...')
+        test_set = []
+        title_df = self.get_title_group()
+        rating_df = self.filtered_rating_df
+        merged_df = pd.merge(title_df, rating_df, on='user', how='inner')
+        print('tt', merged_df['title'])
+        merged_df['title'] = merged_df['title'].isna().astype(int)  # Replace NaN values with 0 and non-NaN values with 1 in the 'title' column
+        print('tt aft', merged_df['title'])
+        grouped = merged_df.groupby('user')
+        for _, group_df in tqdm(grouped):
+            test_data = group_df.iloc[-1:]
+            test_set.append(test_data)
+        test_df = pd.concat(test_set, ignore_index=True)
+        
+        return test_df
     
     def split_train_test(self):
         print('Spliting train, validation, test data ...')
--- main.py
+++ main.py
@@ -16,6 +16,9 @@ from math import sqrt
 import datetime
 import argparse
 import os
+import pickle
+import matplotlib.pyplot as plt
+import pandas as pd
 
 class GraphRec(nn.Module):
 
@@ -67,11 +70,16 @@ def train(model, device, train_loader, optimizer, epoch, best_rmse, best_mae):
     model.train()
     running_loss = 0.0
     for i, data in enumerate(train_loader, 0):
+        # print(i)
         batch_nodes_u, batch_nodes_v, labels_list = data
         optimizer.zero_grad()
+        #print('grad')
         loss = model.loss(batch_nodes_u.to(device), batch_nodes_v.to(device), labels_list.to(device))
-        loss.backward(retain_graph=True)
+        #print(loss)
+        loss.backward(retain_graph=True) #
+        #print('back')
         optimizer.step()
+        #print('step')
         running_loss += loss.item()
         if i % 100 == 0:
             print('[%d, %5d] loss: %.3f, The best rmse/mae: %.6f / %.6f' % (
@@ -88,12 +96,14 @@ def test(model, device, test_loader):
         for test_u, test_v, tmp_target in test_loader:
             test_u, test_v, tmp_target = test_u.to(device), test_v.to(device), tmp_target.to(device)
             val_output = model.forward(test_u, test_v)
-            tmp_pred.append(list(val_output.data.cpu().numpy()))
+            # print('val_o', val_output)
+            tmp_pred.append([i for i in list(np.atleast_1d(val_output.data.cpu().numpy()))])
             target.append(list(tmp_target.data.cpu().numpy()))
     tmp_pred = np.array(sum(tmp_pred, []))
     target = np.array(sum(target, []))
     expected_rmse = sqrt(mean_squared_error(tmp_pred, target))
     mae = mean_absolute_error(tmp_pred, target)
+    print('i-expected_rmse:', expected_rmse)
     return expected_rmse, mae
 
 def main():
@@ -114,12 +124,29 @@ def main():
 
     # load dataset
     ds = InitDataset()
-    train_df, validation_df, test_df = ds.split_train_test() # leave one out # train df 536949; valid df 16597; test df 16597
-    history_u_lists, history_ur_lists = ds.get_history_lists(train_df,'user','item')
-    history_v_lists, history_vr_lists = ds.get_history_lists(train_df,'item','user')
+    # ds.describe_location()
+
+    
+    train_df, validation_df, test_df = ds.split_train_test() # valid and test portion in 0-1
+    history_u_lists, history_ur_lists = ds.get_history_lists(ds.filtered_rating_df,'user','item')
+    #with open("history_u_listsuseritem", 'rb') as f:
+        #history_u_lists = pickle.load(f)
+        #f.close()
+    #with open("history_ur_listsuseritem", 'rb') as f:
+        #history_ur_lists = pickle.load(f)
+        #f.close()
+    
+    history_v_lists, history_vr_lists = ds.get_history_lists(ds.filtered_rating_df,'item','user')
+    #with open("history_u_listsitemuser", 'rb') as f:
+        #history_v_lists = pickle.load(f)
+        #f.close()
+    #with open("history_ur_listsitemuser", 'rb') as f:
+        #history_vr_lists = pickle.load(f)
+        #f.close()
     social_adj_lists = ds.get_social_adj_lists()
 
     batch_size = args.batch_size #
+    #batch_size = 1000
     train_ds = EpinionDataset(train_df)
     train_dl = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
     valid_ds = EpinionDataset(validation_df)
@@ -129,7 +156,12 @@ def main():
     # print(next(iter(train_dl)))
 
     num_users, num_items, num_ratings = ds.describe() # TB debugged - maybe can cause index error
-
+    #print('3 nums:', num_users, num_items, num_ratings)
+    num_users = max(ds.user_dict.values())#train_df.user.unique().max()
+    num_items = max(max(history_u_lists.values()))+1 # ds.filtered_rating_df.item.unique().max() #
+    #print('num_users', num_users)
+    #print('num_items', num_items)
+    
     embed_dim = args.embed_dim
 
     u2e = nn.Embedding(num_users, embed_dim).to(device)
@@ -151,29 +183,141 @@ def main():
 
     # model
     graphrec = GraphRec(enc_u, enc_v_history, r2e).to(device)
-    optimizer = torch.optim.RMSprop(graphrec.parameters(), lr=args.lr, alpha=0.9)
+    if os.path.exists('best_model.pth'):
+        graphrec.load_state_dict(torch.load('best_model.pth'))
 
-    best_rmse = 9999.0
-    best_mae = 9999.0
-    endure_count = 0
+    else:
+        optimizer = torch.optim.RMSprop(graphrec.parameters(), lr=args.lr, alpha=0.9)
 
-    for epoch in tqdm(range(1, args.epochs + 1)):
+        best_rmse = 9999.0
+        best_mae = 9999.0
+        endure_count = 0
 
-        train(graphrec, device, train_dl, optimizer, epoch, best_rmse, best_mae)
-        expected_rmse, mae = test(graphrec, device, test_dl)
-        # please add the validation set to tune the hyper-parameters based on your datasets.
+        for epoch in tqdm(range(1, args.epochs + 1)):
 
-        # early stopping (no validation set in toy dataset)
-        if best_rmse > expected_rmse:
-            best_rmse = expected_rmse
-            best_mae = mae
-            endure_count = 0
-        else:
-            endure_count += 1
-        print("rmse: %.4f, mae:%.4f " % (expected_rmse, mae))
+            train(graphrec, device, train_dl, optimizer, epoch, best_rmse, best_mae)
+            expected_rmse, mae = test(graphrec, device, test_dl)
+            # please add the validation set to tune the hyper-parameters based on your datasets.
 
-        if endure_count > 5:
-            break
+            # early stopping (no validation set in toy dataset)
+            if best_rmse > expected_rmse:
+                best_rmse = expected_rmse
+                best_mae = mae
+                endure_count = 0
+                torch.save(graphrec.state_dict(), 'best_model.pth')
+            else:
+                endure_count += 1
+            print("rmse: %.4f, mae:%.4f " % (expected_rmse, mae))
+
+            if endure_count > 5:
+                break
+
+    ##group evaluation - location
+    test_df_group = ds.create_test_location()
+    grouped = test_df_group.groupby('location')
+    groups_rmse = {}
+    groups_mae = {}
+
+    for l, group_df in grouped: 
+        test_ds_group = EpinionDataset(group_df.drop(columns=['location']))
+        test_dl_group = DataLoader(test_ds_group, batch_size=5, shuffle=True)
+        expected_rmse, mae = test(graphrec, device, test_dl_group)
+        groups_rmse[l] = expected_rmse
+        groups_mae[l] = mae
+        
+    print("groups_rmse\n", groups_rmse, "\n")
+    print("groups_mae\n", groups_mae, "\n") 
+
+
+    # plot
+    keys = list(groups_rmse.keys())
+    rmse_values = list(groups_rmse.values())
+    mae_values = list(groups_mae.values())
+
+    # Sort keys and values based on RMSE values
+    sorted_indices = sorted(range(len(rmse_values)), key=lambda k: rmse_values[k])
+    sorted_keys = [keys[i] for i in sorted_indices]
+    sorted_rmse_values = [rmse_values[i] for i in sorted_indices]
+    sorted_mae_values = [mae_values[i] for i in sorted_indices]
+
+    # Plotting
+    plt.figure(figsize=(10, 6))
+    plt.plot(sorted_keys, sorted_rmse_values, label='RMSE', marker='o')
+    plt.plot(sorted_keys, sorted_mae_values, label='MAE', marker='o')
+    plt.xlabel('States')
+    plt.ylabel('Error')
+    plt.title('RMSE and MAE by State')
+    plt.xticks(rotation=90)
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+    # Get the sample sizes for each state
+    df = pd.read_csv('Epinion/processed_data/profileLoc.csv')
+    sample_sizes = df.groupby('location').size().to_dict()
+
+    # Sort keys and values based on RMSE values
+    sorted_sample_sizes = [sample_sizes.get(key, 0) for key in sorted_keys]
+
+    # Plotting
+    plt.figure(figsize=(10, 6))
+    plt.scatter(sorted_keys, sorted_rmse_values, s=np.array(sorted_sample_sizes) * 2, c='blue', alpha=0.5)
+    plt.scatter(sorted_keys, sorted_mae_values, s=np.array(sorted_sample_sizes) * 2, c='red', alpha=0.5)
+
+    plt.plot(sorted_keys, sorted_rmse_values, color='black', label='RMSE' )
+    plt.plot(sorted_keys, sorted_mae_values, color='gray', label='MAE')
+    plt.xlabel('States')
+    plt.ylabel('Error')
+    plt.title('RMSE and MAE by State with Sample Sizes as Radius')
+    plt.xticks(rotation=90)
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+
+    # Extract keys and values from the dictionaries
+    groups_rmse_social={'Alabama': 0.5373963577683013, 'Alaska': 0.27593869261953724, 'Arizona': 0.23620852289216335, 'Arkansas': 0.13254029062895545, 'California': 0.5692589055027747, 'Colorado': 1.2971548674689404, 'Connecticut': 0.48665693573280916, 'Delaware': 0.1255669625454351, 'District of Columbia': 0.5917687488078925, 'Florida': 0.5675522621810065, 'Georgia': 0.4172036664309281, 'Idaho': 0.2555298758357046, 'Illinois': 0.7258263006282042, 'Indiana': 0.9472199419155124, 'Iowa': 0.8506015990750126, 'Kansas': 0.2596770400822414, 'Kentucky': 0.3708405484165555, 'Louisiana': 0.19833603878306405, 'Maine': 0.1516079311020993, 'Maryland': 0.9461232011465218, 'Massachusetts': 0.2731126899694833, 'Michigan': 0.35949798221181034, 'Minnesota': 0.6710472774734905, 'Mississippi': 1.6938068366279961, 'Missouri': 0.4296854019113816, 'Montana': 0.13763618552498605, 'Nebraska': 0.20623138047472087, 'Nevada': 0.4448484043618812, 'New Hampshire': 0.20672035265681174, 'New Jersey': 0.27903325637829157, 'New Mexico': 0.33848424815825456, 'New York': 0.8140914443246503, 'North Carolina': 1.1563045901221851, 'Ohio': 0.6228422588276756, 'Oklahoma': 0.1317367533827764, 'Oregon': 0.528752599624123, 'Pennsylvania': 0.9398022992132962, 'Rhode Island': 1.3499357543775778, 'South Carolina': 0.7952681403539001, 'South Dakota': 0.26987743260240044, 'Tennessee': 0.7474982338823869, 'Texas': 0.8212816598674262, 'Utah': 0.2944982783432394, 'Vermont': 0.15748644018271538, 'Virginia': 0.8383878997338132, 'Washington': 0.76099729361786, 'Wisconsin': 0.20774243220455857, 'Wyoming': 0.23814062753095064}
+    groups_mae_social={'Alabama': 0.40179202, 'Alaska': 0.24642622, 'Arizona': 0.20037194, 'Arkansas': 0.10279608, 'California': 0.3474253, 'Colorado': 1.0298887, 'Connecticut': 0.40520075, 'Delaware': 0.12556696, 'District of Columbia': 0.59176874, 'Florida': 0.37020144, 'Georgia': 0.32618466, 'Idaho': 0.25552988, 'Illinois': 0.41369843, 'Indiana': 0.5456296, 'Iowa': 0.6975808, 'Kansas': 0.2415124, 'Kentucky': 0.27018446, 'Louisiana': 0.17290497, 'Maine': 0.14970668, 'Maryland': 0.73390824, 'Massachusetts': 0.24007325, 'Michigan': 0.27310598, 'Minnesota': 0.41652727, 'Mississippi': 1.2602792, 'Missouri': 0.33298284, 'Montana': 0.13763618, 'Nebraska': 0.20508885, 'Nevada': 0.32839894, 'New Hampshire': 0.20672035, 'New Jersey': 0.21349378, 'New Mexico': 0.3065132, 'New York': 0.49700263, 'North Carolina': 0.74079424, 'Ohio': 0.40631026, 'Oklahoma': 0.12681627, 'Oregon': 0.292626, 'Pennsylvania': 0.63916546, 'Rhode Island': 1.0119146, 'South Carolina': 0.63400084, 'South Dakota': 0.26987743, 'Tennessee': 0.4497685, 'Texas': 0.5131105, 'Utah': 0.26548305, 'Vermont': 0.15748644, 'Virginia': 0.4742157, 'Washington': 0.5342401, 'Wisconsin': 0.16955525, 'Wyoming': 0.23797405}
+    keys = list(groups_rmse_social.keys())
+    rmse_values = list(groups_rmse.values())
+    rmse_values_social = list(groups_rmse_social.values())
+
+    # Sort keys and values based on RMSE values
+    sorted_indices = sorted(range(len(rmse_values_social)), key=lambda k: rmse_values_social[k])
+    sorted_keys = [keys[i] for i in sorted_indices]
+    sorted_rmse_values = [rmse_values[i] for i in sorted_indices]
+    sorted_rmse_values_social = [rmse_values_social[i] for i in sorted_indices]
+
+    # Plotting
+    plt.figure(figsize=(10, 6))
+    plt.plot(sorted_keys, sorted_rmse_values_social, label='RMSE_Social', marker='o')
+    plt.plot(sorted_keys, sorted_rmse_values, label='RMSE', marker='o')
+    plt.xlabel('States')
+    plt.ylabel('Error')
+    plt.title('RMSE with and w/o Social Network')
+    plt.xticks(rotation=90)
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+
+    ##group evaluation - title
+    test_df_group = ds.create_test_title()
+    grouped = test_df_group.groupby('title')
+    groups_rmse = {}
+    groups_mae = {}
+
+    for l, group_df in grouped: 
+        print(l)
+        test_ds_group = EpinionDataset(group_df.drop(columns=['title']))
+        test_dl_group = DataLoader(test_ds_group, batch_size=5, shuffle=True)
+        expected_rmse, mae = test(graphrec, device, test_dl_group)
+        groups_rmse[l] = expected_rmse
+        groups_mae[l] = mae
+        
+    print("groups_rmse\n", groups_rmse, "\n")
+    print("groups_mae\n", groups_mae, "\n")        
+    
 
 if __name__ == '__main__':
     main()
